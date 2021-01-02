370f06ff295ecd45c577247e8334b378b6d79c06
Craig Miller <craigmiller160@gmail.com>
2021-01-02 16:37:41 -0500

    Stubbed out tests

---
+++ test/stringEqualIgnoreWhitespace.test.ts
@@ -0,0 +1,19 @@
+import '../src';
+
+describe('stringEqualIgnoreWhitespace', () => {
+    it('is equal', () => {
+        throw new Error();
+    });
+
+    it('is not equal', () => {
+        throw new Error();
+    });
+
+    it('expected not string', () => {
+        throw new Error();
+    });
+
+    it('received not string', () => {
+        throw new Error();
+    });
+});
